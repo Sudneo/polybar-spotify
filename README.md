a85da98ab332e50921966261c8ea61b46874888b
sudneo <mucci.daniele@gmail.com>
2019-11-27 00:17:43 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # polybar-spotify
 Small script to get Spotify control to work with Polybar
+
+## Spotify Control and Polybar
+
+In order to have this to work you need to have
+
+* Polybar working
+* Python `dbus` module installed
+* Python 3
+
+## Idea
+
+The idea is to have a simple way to see Spotify music playing and simple controls to pause/play
+songs and to go to previous/next song.
+
+## Limitations
+
+The main limitation is that in order to get the name of the song being played and the play/pause
+icon correct, the script needs to be executed often.
+The second limitation is that Spotify application doesn't expose the current position in the song,
+so we cannot show a fancy progress bar of the song.
+
+## Configuration
+
+I deliberately wrote a script that could do all I needed and then split the functionalities in
+different polybar modules.
+
+The result was something like the following:
+
+```
+[bar/top]
+
+[...]
+modules-left = spotify-song spotify-backward spotify-status spotify-forward
+
+[module/spotify-song]
+type = custom/script
+exec = ~/polybar-scripts/spotify-cmd.py --playing --trim_or_pad 50
+interval = 0.1
+format = <label>
+format-foreground = #fff
+format-background = #773f3f3f
+format-underline = #c9665e
+format-padding = 4
+label = %{T4}%output%%{T-}
+
+[module/spotify-backward]
+type = custom/script
+exec = python3 -c 'print("\uf048")'
+click-left = ~/polybar-scripts/spotify-cmd.py --previous
+format-padding = -1
+
+[module/spotify-status]
+type = custom/script
+exec = ~/polybar-scripts/spotify-cmd.py --playpause_icon
+click-left = ~/polybar-scripts/spotify-cmd.py --playpause
+interval = 0.1
+format-padding = -1
+
+[module/spotify-forward]
+type = custom/script
+exec = python3 -c 'print("\uf051")'
+click-left = ~/polybar-scripts/spotify-cmd.py --next
+format-padding = -1
+```
+
+Few things to notice:
+
+* The custom font usage in the `spotify-song` block is not casual. Since the blocks are separated,
+  the best result is obtained with monospace fonts, so that the width of the block is fixed.
+* `print("\uf048")` and `print("\uf051")` commands are just printing icons for previous/next song.
+  This can be done in the script itself with a little addition.
+
+## Outcome
+
+When a song is playing, the result is like this:
+
+[[playing.png]]
+
+When a song is paused, the result is like this:
+
+[[paused.png]]
---
+++ spotify-cmd.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+import dbus
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--playing', action='store_true', help="Returns the info about the song currently playing.")
+    group.add_argument('--playpause_icon', action='store_true', help="Prints the icon opposite to status (Play if music"
+                                                                     " is stopped, Pause if music is playing.")
+    group.add_argument('--playpause', action='store_true', help="Plays/Pauses the current song.")
+    group.add_argument('--next', action='store_true', help="Play next song if available, otherwise do nothing.")
+    group.add_argument('--previous', action='store_true', help="Play previous song if available, otherwise do nothing.")
+    parser.add_argument('--trim_or_pad', type=int, metavar="max_length", help="Max length for the song string")
+    args = parser.parse_args()
+    return args
+
+
+def trim_or_pad(string, length):
+    if len(string) > length:
+        return string[:length]
+    elif len(string) < length:
+        return string + " " * (length - len(string))
+    else:
+        return string
+
+
+def main():
+    args = get_args()
+    try:
+        session_bus = dbus.SessionBus()
+        spotify_bus = session_bus.get_object(
+            'org.mpris.MediaPlayer2.spotify',
+            '/org/mpris/MediaPlayer2'
+        )
+
+        spotify_properties = dbus.Interface(
+            spotify_bus,
+            'org.freedesktop.DBus.Properties'
+        )
+        interface = dbus.Interface(spotify_bus, dbus_interface='org.mpris.MediaPlayer2.Player')
+        if args.playing:
+            # We need to print Song name - Artist (Album)
+            metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+            song_name = metadata['xesam:title']
+            artist = metadata['xesam:artist'][0]
+            album = metadata['xesam:albumArtist'][0]
+            dash = '\u2014'
+            status_icon = '\uf1bc'
+            if args.trim_or_pad:
+                max_length = args.trim_or_pad
+                print(trim_or_pad(f'{status_icon} {song_name} {dash} {artist} ({album})', max_length))
+            else:
+                print(f'{status_icon} {song_name} {dash} {artist} ({album})')
+            exit(0)
+        elif args.playpause:
+            interface.PlayPause()
+            exit(0)
+        elif args.next:
+            interface.Next()
+            exit(0)
+        elif args.previous:
+            interface.Previous()
+            exit(0)
+        elif args.playpause_icon:
+            status = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
+            if status == 'Playing':
+                print('\uf04c')
+            else:
+                print('\uf04b')
+    except Exception as error:
+        exit(1)
+
+
+main()
