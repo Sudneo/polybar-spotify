dcf4fd31c5538ac1224e6449cb8d628596cbde6f
sudneo <mucci.daniele@gmail.com>
2019-11-30 14:04:09 +0200

    Better error handling, all icons are printed by the script

--- spotify-cmd.py
+++ spotify-cmd.py
@@ -11,7 +11,9 @@ def get_args():
                                                                      " is stopped, Pause if music is playing.")
     group.add_argument('--playpause', action='store_true', help="Plays/Pauses the current song.")
     group.add_argument('--next', action='store_true', help="Play next song if available, otherwise do nothing.")
+    group.add_argument('--next_icon', action='store_true', help="Print previous symbol.")
     group.add_argument('--previous', action='store_true', help="Play previous song if available, otherwise do nothing.")
+    group.add_argument('--previous_icon', action='store_true', help="Print next symbol.")
     parser.add_argument('--trim_or_pad', type=int, metavar="max_length", help="Max length for the song string")
     args = parser.parse_args()
     return args
@@ -42,6 +44,14 @@ def main():
         interface = dbus.Interface(spotify_bus, dbus_interface='org.mpris.MediaPlayer2.Player')
         if args.playing:
             # We need to print Song name - Artist (Album)
+            status = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
+            if args.trim_or_pad:
+                max_length = args.trim_or_pad
+            if status not in ['Playing', 'Paused', 'Stopped']:
+                if args.trim_or_pad:
+                    print(trim_or_pad('Spotify is not running'), max_length)
+                else:
+                    print('Spotify is not Running')
             metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
             song_name = metadata['xesam:title']
             artist = metadata['xesam:artist'][0]
@@ -49,7 +59,6 @@ def main():
             dash = '\u2014'
             status_icon = '\uf1bc'
             if args.trim_or_pad:
-                max_length = args.trim_or_pad
                 print(trim_or_pad(f'{status_icon} {song_name} {dash} {artist} ({album})', max_length))
             else:
                 print(f'{status_icon} {song_name} {dash} {artist} ({album})')
@@ -69,6 +78,10 @@ def main():
                 print('\uf04c')
             else:
                 print('\uf04b')
+        elif args.next_icon:
+            print("\uf051")
+        elif args.previous_icon:
+            print("\uf048")
     except Exception as error:
         exit(1)
 
